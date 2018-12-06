f2e1b5198f1f1307c65a72820be21852520e3475
fengzilong <feng_zilong@163.com>
2018-12-06 12:00:17 +0800

    Initial Commit

---
+++ oxo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  oxo
+//
+//  Created by zilong on 2018/12/6.
+//  Copyright © 2018年 zilong. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
